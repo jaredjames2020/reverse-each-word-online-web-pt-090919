79068a7bb704916aa198596829aecb8adb08031c
Jared S <jaredjames2020@gmail.com>
2019-09-16 22:28:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 def reverse_each_word(sentence)
   array = sentence.split
-  array.reverse
-  binding.pry
-
+  array.each do |words|
+    puts words.reverse
+    binding.pry
+  end
 end
