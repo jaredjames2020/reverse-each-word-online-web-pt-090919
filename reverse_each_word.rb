d18d146db24741ff66a1aae47135a9f75dfa563e
Jared S <jaredjames2020@gmail.com>
2019-09-16 22:34:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 def reverse_each_word(sentence)
   rev_sentence = ""
-  array = sentence.split(" ")
-  array.each do |words|
-    rev_sentence << " " + words.reverse
-    #binding.pry
-  end
+  # array = sentence.split(" ")
+  # array.each do |words|
+  #   rev_sentence << " " + words.reverse
+  #   #binding.pry
+  # end
   array = sentence.split(" ")
   array.collect do |words|
     rev_sentence << " " + words.reverse
